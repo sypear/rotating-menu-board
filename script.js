599c91f11c9673e33e683a8e8fb503e61e8e5655
seungyeon.bae <bsyeon015@gmail.com>
2022-09-20 21:50:18 +0900

    🔥필요없는 주석 삭제

--- script.js
+++ script.js
@@ -16,24 +16,3 @@ moveRightButton.addEventListener('click', function(e) {
 
     menuList.style.transform = `translate(-50%, -50%) rotateY(${currentAngle}deg)`;
 });
-
-// const center = document.querySelector('.list-item'); // list-item이란 클래스를 가진 요소를 선택
-//         const items = document.querySelectorAll('.list-item li');
-//         const angle = 360 / items.length; // 아이템이 회전해야 하는 각도
-//         let currAngle = 0; // 현재 각도
-
-//         document.addEventListener('click', function (event) {
-//             // window.innerHeight : 브라우저 view port의 넓이
-//             // event.clientX : view port의 왼쪽에서 X축만큼 얼마나 떨어져있는지 수치를 반환
-
-//             // 클릭 이벤트가 발생했을때 왼쪽에서 X축만큼 얼마나 떨어져 있느냐? 
-//             if (window.innerWidth / 2 < event.clientX) {
-//                 // 이벤트가 발생한 위치가 화면을 반으로 나눴을 때 중간값보다 작은 경우
-//                 currAngle -= angle;
-//             } else {
-//                 // 이벤트가 발생한 위치가 화면을 반으로 나눴을 때 중간값보다 큰 경우
-//                 currAngle += angle;
-//             }
-
-//             center.style.transform = `translate(-50%, -50%) rotateY(${currAngle}deg)`;
-//         })
